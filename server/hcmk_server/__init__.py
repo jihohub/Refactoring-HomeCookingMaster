eafd06265b6475af71cbfbe53bc433808c0b2be9
[2기-2반] 김한별 <hanstar07@gmail.com>
2021-11-19 11:18:23 +0000

    [BACK] MR back -> dev

--- server/hcmk_server/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_restx import Api
-
-# from flask_cors import CORS
-import os
-import sys
-from dotenv import load_dotenv
