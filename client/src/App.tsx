2f3b38f92e1fa3603a50e8f969adbcd6fc37d11f
Jiho <diorpark@gmail.com>
2021-11-22 13:02:58 +0900

    task: 반응형 상단바, 스크롤 스냅, 상단바 숨김

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,27 @@
 /** @jsxImportSource @emotion/react */
 import { css, jsx } from "@emotion/react";
 
+import { Provider } from "react-redux";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import Router from "./Router";
+import hideHeaderSlice from "./components/Common/hideHeaderSlice";
 import "./App.css";
 
 function App() {
+    const rootReducer = combineReducers({
+        hideHeaderSlice: hideHeaderSlice
+    });
+
+    const store = configureStore({
+        reducer: rootReducer
+    });
+
     return (
-      <div>
-        <Router />
-      </div>
+        <Provider store={store}>
+            <div>
+                <Router />
+            </div>
+        </Provider>
     );
 }
 
