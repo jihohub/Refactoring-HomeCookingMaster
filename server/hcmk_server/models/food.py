87579b7b230548d6491fc50c8c21f743e06e89ab
[2기-1반]김희재 <gmlwo0109@gmail.com>
2021-11-24 10:50:29 +0900

    refactor: edit DB table

--- server/hcmk_server/models/food.py
+++ server/hcmk_server/models/food.py
@@ -6,36 +6,28 @@ class Food(db.model):
     __tablename__ = "food"
     '''
     name = 음식 이름
-    img = 음식 사진 주소
     category_l = 대분류
     category_m = 중분류
     category_s = 소분류
-    views = 조회 수
     '''
 
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     name = db.Column(db.String(32), nullable=False)
-    img = db.Column(db.String(1024))
     category_l = db.Column(db.String(32))
     category_m = db.Column(db.String(32))
     category_s = db.Column(db.String(32))
-    views = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, name, img, category_l, category_m, category_s):
+    def __init__(self, name, category_l, category_m, category_s):
         self.name = name
-        self.img = img
         self.category_l = category_l
         self.category_m = category_m
         self.category_s = category_s
-        self.views = 0
 
     def to_dict(self):
         return {
             "id": self.id,
             "name": self.name,
-            "img": self.img,
             "category_l": self.category_l,
             "category_m": self.category_m,
             "category_s": self.category_s,
-            "views": self.views,
         }
--- server/hcmk_server/models/user.py
+++ server/hcmk_server/models/user.py
@@ -8,7 +8,6 @@ class User(db.Model):
     ''' 
     email = 아이디(이메일)       
     password = 비밀번호
-    name = 사용자 이름
     nickname = 닉네임
     img = 프로필 사진 주소
     intro = 한 줄 소개
@@ -17,16 +16,14 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     email = db.Column(db.String(256), nullable=False, unique=True)
     password = db.Column(db.String(64), nullable=False)
-    name = db.Column(db.String(32), nullable=False)
     nickname = db.Column(db.String(128))
     img = db.Column(db.String(1024))
     intro = db.Column(db.Text)
     exp = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, email, password, name, nickname, img, intro):
+    def __init__(self, email, password, nickname, img, intro):
         self.email = email
         self.password = password
-        self.name = name
         self.nickname = nickname
         self.img = img
         self.intro = intro
@@ -37,9 +34,8 @@ class User(db.Model):
         return {
             "id": self.id, 
             "email": self.email, 
-            "name": self.name,
             "nickname": self.nickname, 
-            # "img": self.img, 
-            # "intro": self.intro, 
-            # "exp": self.exp, 
+            "img": self.img, 
+            "intro": self.intro, 
+            "exp": self.exp, 
             }
