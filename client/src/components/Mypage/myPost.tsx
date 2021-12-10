28c38ba624357c1f7b4cb013922de7410c27067c
unknown <diorpark@gmail.com>
2021-12-10 18:49:46 +0900

    fix: 콘솔창 정리

--- client/src/components/Mypage/myPost.tsx
+++ client/src/components/Mypage/myPost.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import { css, jsx } from "@emotion/react";
+import { css } from "@emotion/react";
 import Typography from '@mui/material/Typography';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
