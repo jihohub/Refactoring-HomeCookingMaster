d9afa1eb66836445115e8101d5050fe1abac87e4
unknown <diorpark@gmail.com>
2021-12-11 14:31:40 +0900

    fix: 리뷰 게시판 수직 정렬 수정

--- client/src/components/Recipe/ReviewList.tsx
+++ client/src/components/Recipe/ReviewList.tsx
@@ -55,7 +55,7 @@ function Row(props: any) {
                                             <ImageIcon fontSize="small" />
                                         </IconButton>
                                     </Box>
-                                    <Box sx={{ width: "90%" }}>
+                                    <Box sx={{ width: "90%", margin: "auto 0" }}>
                                         <Typography
                                             sx={{ fontFamily: "Elice" }}
                                         >
