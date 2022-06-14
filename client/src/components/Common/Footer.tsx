cca6c1caf135ea7a370e215ce8285153b404724e
Jiho Park <diorpark@gmail.com>
2022-06-14 23:20:14 +0900

    refactor: 헤더바, 푸터 디자인 수정

--- client/src/components/Common/Footer.tsx
+++ client/src/components/Common/Footer.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Link from "next/link";
 import Image from "next/image"
 import { Box, Typography } from "@mui/material";
 import avatar from "../../../public/assets/avatar.png";
@@ -9,24 +8,10 @@ import styles from "./Footer.module.scss";
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <a>Copyright 2022. 집밥꼬꼬선생 All right reserved.</a>
-      <Link href="https://github.com/jihohub/Refactoring-HomeCookingMaster">
-        <a>
-          GitHub
-        </a>
-      </Link>
+      <p>Copyright 2022. 집밥꼬꼬선생 All right reserved.</p>
+      <a target="_blank" href="https://github.com/jihohub/Refactoring-HomeCookingMaster" className={styles.link}>GitHub</a>
     </div>
   );
 };
 
 export default Footer;
-
-const Foot = styled(Typography)({
-  fontFamily: "Elice",
-  marginLeft: 10,
-  marginRight: 10,
-  cursor: "pointer",
-  "&:hover": {
-    color: "white",
-  },
-});
