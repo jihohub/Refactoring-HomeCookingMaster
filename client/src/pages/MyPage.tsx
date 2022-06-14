bd5f8bb4baf67e129dadce3658a72a4d5b4882f3
Jiho Park <diorpark@gmail.com>
2022-06-14 23:40:10 +0900

    refactor: 마이페이지 파일명 수정

--- client/src/pages/MyPage.tsx
+++ client/src/pages/MyPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useRouter } from "next/router";
 import IntroMe from "../components/Mypage/IntroMe";
 import MyPost from "../components/Mypage/myPost";
