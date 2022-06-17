ac3cb7b5f5dab1c451563bdd8d71fd4c6a63d73c
Jiho Park <diorpark@gmail.com>
2022-06-18 04:20:02 +0900

    refactor: 회원가입 페이지 디자인

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,9 +1,10 @@
 import { Router } from "@mui/icons-material";
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import useSignup from "../hooks/Auth/useSignup";
 import useValidateEmail from "../hooks/Auth/useValidateEmail";
 import useValidateNickname from "../hooks/Auth/useValidateNickname";
+import styles from "./register.module.scss";
 
 export default function App() {
   const { mutate: signup, isLoading: signupLoading } = useSignup();
@@ -21,6 +22,19 @@ export default function App() {
   const password = watch("password");
   const nickname = watch("nickname");
   const img = watch("img");
+  const [profileImage, setProfileImage] = useState<String | ArrayBuffer | null>(
+    ""
+  );
+
+  const onUploadImage = (e: any) => {
+    let reader = new FileReader();
+    setProfileImage(e.target.files[0]);
+    const previewImage = e.target.files[0];
+    reader.onloadend = () => {
+      setProfileImage(reader.result);
+    };
+    reader.readAsDataURL(previewImage);
+  }
 
   const onSubmit = () => {
     const formData = new FormData();
@@ -31,72 +45,129 @@ export default function App() {
     signup(formData);
   };
 
-  
-
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <label>이메일</label>
-      <input
-        type="text"
-        {...register("email", {
-          required: true,
-          pattern: {
-            value: /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i,
-            message: "이메일 형식이 아닙니다.",
-          },
-        })}
-      />
-      {errors.email && <p>{errors.email.message}</p>}
-      <button onClick={() => valEmail(email)}></button>
-      <label>비밀번호</label>
-      <input
-        type="password"
-        {...register("password", {
-          required: "비밀번호는 필수입니다.",
-          pattern: {
-            value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/i,
-            message: "숫자/영문자 조합으로 8~16자로 입력해주세요.",
-          },
-        })}
-      />
-      {errors.password && <p>{errors.password.message}</p>}
+    <div className={styles.root}>
+      <form className={styles.register} onSubmit={(e) => e.preventDefault()}>
+        <div className={styles.register__section}>
+          <label className={styles.register__label}>이메일</label>
+          <input
+            type="text"
+            className={styles.register__input}
+            {...register("email", {
+              required: "이메일은 필수입니다.",
+              pattern: {
+                value: /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i,
+                message: "이메일 형식이 아닙니다.",
+              },
+            })}
+          />
 
-      <label>비밀번호 확인</label>
-      <input
-        type="password"
-        {...register("confirm_password", {
-          required: "비밀번호 확인은 필수입니다.",
-          validate: (value) => {
-            return (
-              value === getValues("password") || "비밀번호가 일치하지 않습니다."
-            );
-          },
-        })}
-      />
-      {errors.confirm_password && <p>{errors.confirm_password.message}</p>}
+          <button
+            onClick={() => valEmail(email)}
+            className={styles.register__button}
+          >
+            중복확인
+          </button>
+        </div>
+        <div className={styles.register__section}>
+          {errors.email && (
+            <p className={styles.register__error}>{errors.email.message}</p>
+          )}
+        </div>
+        <div className={styles.register__section}>
+          <label className={styles.register__label}>비밀번호</label>
+          <input
+            type="password"
+            className={styles.register__input}
+            {...register("password", {
+              required: "비밀번호는 필수입니다.",
+              pattern: {
+                value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/i,
+                message: "숫자/영문자 조합으로 8~16자로 입력해주세요.",
+              },
+            })}
+          />
+        </div>
+        <div className={styles.register__section}>
+          {errors.password && (
+            <p className={styles.register__error}>{errors.password.message}</p>
+          )}
+        </div>
+        <div className={styles.register__section}>
+          <label className={styles.register__label}>비밀번호 확인</label>
+          <input
+            type="password"
+            className={styles.register__input}
+            {...register("confirm_password", {
+              required: "비밀번호 확인은 필수입니다.",
+              validate: (value) => {
+                return (
+                  value === getValues("password") ||
+                  "비밀번호가 일치하지 않습니다."
+                );
+              },
+            })}
+          />
+        </div>
+        <div className={styles.register__section}>
+          {errors.confirm_password && (
+            <p className={styles.register__error}>
+              {errors.confirm_password.message}
+            </p>
+          )}
+        </div>
+        <div className={styles.register__section}>
+          <label className={styles.register__label}>닉네임</label>
+          <input
+            type="nickname"
+            className={styles.register__input}
+            {...register("nickname", {
+              required: "닉네임은 필수입니다.",
+              maxLength: {
+                value: 10,
+                message: "닉네임은 10자 이하로 설정해주세요.",
+              },
+            })}
+          />
 
-      <label>닉네임</label>
-      <input
-        type="nickname"
-        {...register("nickname", {
-          required: "닉네임은 필수입니다.",
-          maxLength: {
-            value: 10,
-            message: "닉네임은 10자 이하로 설정해주세요.",
-          },
-        })}
-      />
-      {errors.nickname && <p>{errors.nickname.message}</p>}
-      <button onClick={() => valNickname(nickname)}></button>
-
-      <label>프로필 사진</label>
-      <input
-        type="file"
-        accept="image/png, image/jpeg"
-        {...register("img")}
-      />
-
-      <input type="submit" onClick={handleSubmit(onSubmit)} />
-    </form>
+          <button
+            onClick={() => valNickname(nickname)}
+            className={styles.register__button}
+          >
+            중복확인
+          </button>
+        </div>
+        <div className={styles.register__section}>
+          {errors.nickname && (
+            <p className={styles.register__error}>{errors.nickname.message}</p>
+          )}
+        </div>
+        <div className={styles.register__section}>
+          <label className={styles.register__label}>프로필 사진</label>
+          <input
+            type="file"
+            accept="image/png, image/jpeg"
+            className={styles.root}
+            {...register("img")}
+            onChange={(e) => onUploadImage(e)}
+          />
+          {profileImage && (
+            <img
+              src={profileImage}
+              alt="profile image"
+              className={styles.register__image}
+            />
+          )}
+        </div>
+        <div className={styles.register__section}>
+          <input
+            type="submit"
+            value="가입하기"
+            onClick={handleSubmit(onSubmit)}
+            className={styles.register__submitButton}
+          />
+        </div>
+      </form>
+    </div>
   );
 }
