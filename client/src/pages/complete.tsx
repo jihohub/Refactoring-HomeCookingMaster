959b29b059c706036f06cba754dfdc3d3dcf5081
Jiho Park <diorpark@gmail.com>
2022-07-13 18:48:24 +0900

    refactor: 가입완료 페이지 수정

--- client/src/pages/complete.tsx
+++ client/src/pages/complete.tsx
@@ -1,20 +1,9 @@
-import { Router } from "@mui/icons-material";
-import React, { useRef } from "react";
-import { useForm } from "react-hook-form";
-import useSignup from "../hooks/Auth/useSignup";
-import useValidateEmail from "../hooks/Auth/useValidateEmail";
-import useValidateNickname from "../hooks/Auth/useValidateNickname";
+import React from "react";
 import { useRecoilValue } from "recoil";
 import { registerInfo } from "../atom/registerInfo";
-
-
 
 export default function App() {
   const registered = useRecoilValue(registerInfo);
 
-  return (
-    <>
-      <p>{`${registered.nickname}님 가입이 완료되었습니다.`}</p>
-    </>
-  );
+  return <p>{`${registered.nickname}님 가입이 완료되었습니다.`}</p>
 }
